4734b4876470c6659554a9c1da457cb94b21fde9
Amit <amitzmn@gmail.com>
2018-11-02 17:10:13 -0400

    Changed API to Space X launch years

--- app.js
+++ app.js
@@ -10,35 +10,35 @@ app.appendChild(container); //to append CONTAINER to app root
 
   var req = new XMLHttpRequest(); //create request
 
-  req.open('GET', 'https://ghibliapi.herokuapp.com/films', true); //using GET request on the URL endpoint to open a new connection
+  req.open('GET', 'https://api.spacexdata.com/v3/launches', true); //using GET request on the URL endpoint to open a new connection
 
   req.onload = function () {
     //JSON data is accessed here
     var data  = JSON.parse(this.response); //to parse response and make DATA variable that has all the JSON as an ARRAY of JS OBJECTS.
     //IF statment BELOW to have success for any status-code response between 200-300 and a NOT-FOUND log out code outside those ranges i.e. if incorrect URL is used/typed in.
     if (req.status >= 200 && req.status < 400) {
-      data.forEach(movie => {
-        // console.log(movie.title);
+      data.forEach(spacex => {
+        // console.log(spacex.mission_name);
         const box = document.createElement('div'); //created div with BOX CLASS
         box.setAttribute('class', 'box');
 
         const h1 = document.createElement('h1'); //created h1 set to text content to the TITLE from data from website's/JSON data.
-        h1.textContent = movie.title;
+        h1.textContent = spacex.mission_name;
 
         const desc = document.createElement('desc'); //created DESC and set textContent to the DESCRIPTION from data from website's/JSON data.
-        movie.description = movie.description.substring(0, 150); //substring and 150 used here to limit description and keep all BOX sizes the same.
-        desc.textContent = `${movie.description}...`;
+        spacex.launch_year = spacex.launch_year.substring(0, 150); //substring and 150 used here to limit description and keep all BOX sizes the same.
+        desc.textContent = `${spacex.launch_year}...`;
 
         container.appendChild(box); //to append box to the container
 
         //For each Box, it will contain the H1 and DESC so below code is to append them to the BOX.
-        box.appenChild(h1);
-        box.appenChild(desc);
+        box.appendChild(h1);
+        box.appendChild(desc);
 
       });
     } else {
       // console.log('Sorry There is an Error');
-      const err = document,createElement('errNotice');
+      const err = document.createElement('errNotice');
       err.textContent = `Error`;
       app.appendChild(err);
     }
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Practice/learning making API calls using Javascript
+Practice/learning making API calls using vanilla Javascript & HTTP requests
